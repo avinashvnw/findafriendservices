0ada403f6a46458685869fe7477698dd0e9cdb15
Avinash Varma Mandapati <avinash@Avinashs-MacBook-Pro.local>
2021-12-05 11:27:20 -0600

    first commit

---
+++ src/main/java/com/example/findafriend/findafriendservices/FindafriendservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.findafriend.findafriendservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindafriendservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindafriendservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/findafriend/findafriendservices/FindafriendservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findafriend.findafriendservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindafriendservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
